a90f9e5546627d1e1ba4d22f936f3f62f576e2cc
Rod Paperfon <admin@paperfon.com>
2019-10-30 08:54:03 +0100

    Adding input features and letter grades

--- Book.cs
+++ Book.cs
@@ -8,10 +8,18 @@ namespace GradeBook
         public Book(string name)
         {
             grades = new List<double>();
+            Name = name;
         }
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
 
         public Statistics GetStatistics()
@@ -31,10 +39,48 @@ namespace GradeBook
 
             result.Average /= grades.Count;
 
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
         }
 
         private List<double> grades;
-        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
+        }
+        public string name;
     }
-}   
+}
--- Program.cs
+++ Program.cs
@@ -8,15 +8,45 @@ namespace GradeBook
         static void Main(string[] args)
         {
 
-            var book = new Book("ruby");
-            book.AddGrade(89.7);
-            book.AddGrade(77.7);
-            book.AddGrade(77.7);
+            var book = new Book("Ruby's Grade Book");
+            //book.AddGrade(89.7);
+            //book.AddGrade(77.7);
+            //book.AddGrade(77.7);
+
+            do
+            {
+                Console.WriteLine("Please insert your grades. Quit with Q:");
+                var input = Console.ReadLine();
+                if (input == "Q") { break; }
+
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("***");
+                }
+
+            } while (true);
+
             var stats = book.GetStatistics();
 
-            Console.WriteLine($"Your average grade is {stats.Low:N1}");
+            Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"Your highest grade is {stats.High:N1}");
-            Console.WriteLine($"Your lowest grade is {stats.Average:N1}");
+            Console.WriteLine($"Your lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"Your average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
 
         }
     }
